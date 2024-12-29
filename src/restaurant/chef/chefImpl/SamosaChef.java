09d542748c43fd2f928bc62116e73aabbed82642
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-12-29 12:55:48 +0530

    Restaurant order service factory method logic. Where chef is automatically preparing dish based on its speciality.

---
+++ src/restaurant/chef/chefImpl/SamosaChef.java
@@ -0,0 +1,10 @@
+package restaurant.chef.chefImpl;
+
+import restaurant.chef.Chef;
+
+public class SamosaChef extends Chef {
+    @Override
+    public String prepareDish() {
+        return "Samosa is ready.";
+    }
+}
